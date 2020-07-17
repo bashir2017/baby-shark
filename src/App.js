5093c09be95fb43373fa572c7424b9419d6c1738
bashir2017 <bashiralhanshali@yahoo.com>
2020-07-16 22:52:50 -0400

    n/a

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import BabyShark from "./components/BabyShark";
 
 function App() {
   return (
-    <div>
+    <div className='party-time'>
       <NavBar />
       <BabyShark />
       <Footer />
