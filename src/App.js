0629087a8cc8d5759faa3f7f58f0c8ac53b59964
senadakadric <skadric@fordham.edu>
2020-07-16 22:36:56 -0400

    app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
-import BabyShark from './components/BabyShark'
+import React from "react";
+import Sound from "react-sound";
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import soundFile from "./audio/babyShark.mp3";
+import BabyShark from "./components/BabyShark";
+// import "animate.min.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <NavBar />
       <BabyShark />
       <Footer />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 
 function NavBar() {
-    return( <div> 
-        <button>Surprise Me</button>
-        <button>Baby Goat Remix</button>
-    </div>)
+  return (
+    <div>
+      <button class="ui button">Surprise Me</button>
+      <button class="ui button">Baby Goat Remix</button>
+    </div>
+  );
 }
 
 export default NavBar;
